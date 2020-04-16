053dd241ea0f4e25f584ea2f5388f457c8af9859
ericlima <eric.george.lima@gmail.com>
2020-04-16 09:23:57 +0100

    1 file modified

--- components/App.js
+++ components/App.js
@@ -1,12 +1,9 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends React.Component {
   render() {
-    return (
-      <div>
-      App
-      </div>
-    )
+    return <div>App</div>;
   }
 }
 
